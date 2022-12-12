25cc9d3547ac479818da20971be8b093492f0f8c
sudwsantos <sudwsantos2@gmail.com>
2022-12-12 18:32:20 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./views/Home";
+import Favoritos from "./views/Favoritos";
+
+export default function App() {
+  
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
