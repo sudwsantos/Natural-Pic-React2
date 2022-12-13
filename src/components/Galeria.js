1de3fd4b3ebbedc0294bbc23c48af22c678a4e7a
sudwsantos <sudwsantos2@gmail.com>
2022-12-13 08:53:33 -0300

    imagens listas

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,11 +1,27 @@
+import React from "react";
 import "../assets/css/galeria.css";
+import Allfotos from "../fotos.json";
 import Heart from "./Heart";
 
-export default function Home() {
+const Galeria = () => {
+  const data = Allfotos.photos;
 
   return (
     <div className="galeria grid-columns-5 p-3">
+      {data.map((foto) => {
+        return (
+          <div className="image">
+            <p>Prueba texto</p>
+            <img src={foto.src.tiny} alt='imagen fondo' />
+            <Heart />
+          </div>
+        );
 
+      
+        })}
+        
     </div>
   );
-}
+};
+
+export default Galeria;
